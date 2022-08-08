f906c1798e46c7617d39e0633aaacdd2445d1b43
Leonardo Medina <leonarmed@gmail.com>
2022-08-08 22:51:02 +0000

    Bug fixing href in social network

--- src/js/app.js
+++ src/js/app.js
@@ -62,10 +62,10 @@ function render(variables = {}) {
               github ? github : ""
             }"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/${
-              linkedin ? github : ""
+              linkedin ? linkedin : ""
             }"><i class="fab fa-linkedin"></i></a></li>
             <li><a href="https://instagram.com/${
-              instagram ? github : ""
+              instagram ? instagram : ""
             }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
